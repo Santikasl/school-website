728f6c7e92c902eaea71b6c13157dc94875851e5
Santikasl <zevs22811@gmail.com>
2022-12-25 17:34:01 +0300

    add search

--- school/schoolapp/views.py
+++ school/schoolapp/views.py
@@ -18,17 +18,29 @@ class NewsView(ListView):
 
     def get_queryset(self):
         order_field = self.request.GET.get('order_field')
+        query = self.request.GET.get('search')
         if order_field:
             self.queryset = self.queryset.order_by(order_field)
+            return self.queryset
+        elif query:
 
-        return self.queryset
+            object_list = News.objects.filter(title__iregex=query)
+            return object_list
+        else:
+            return self.queryset
 
     def get(self, request):
         super().get(request)
         order_field = self.request.GET.get('order_field')
-        context = self.get_context_data()
-        context['order_field'] = order_field
-        return self.render_to_response(context)
+        if order_field:
+            context = self.get_context_data()
+            context['order_field'] = order_field
+            return self.render_to_response(context)
+        else:
+            context = self.get_context_data()
+            context['order_field'] = "#"
+            return self.render_to_response(context)
+
 
 
 
