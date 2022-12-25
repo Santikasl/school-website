b7aec4b9c325c5f98b9451fe0aec08cdc13fca2d
Santikasl <zevs22811@gmail.com>
2022-12-25 18:07:30 +0300

    add img gallery

--- school/schoolapp/static/js/gallery.js
+++ school/schoolapp/static/js/gallery.js
@@ -1,80 +1,24 @@
-    const mainImg = document.querySelector(".main-img");
-const images = document.querySelectorAll(".gallery-wrapper img");
-const closeBtn = document.querySelector(".close");
-const nextBtn = document.querySelector(".next");
-const prevBtn = document.querySelector(".prev");
+var modal = document.getElementById('myModal');
 
-nextBtn.addEventListener("click", nextImg);
-prevBtn.addEventListener("click", prevImg);
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-closeBtn.addEventListener("click", (e) => {
-	mainImg.style.display = "none";
-	mainImg.classList.remove("active");
-});
-
-// Creating Animation For Image Transition
-
-const imgStyles = `animation: imgTransition 0.450s  ease-in-out`;
-
-let showImg = document.querySelector(".main-img img");
-
-let imagesArray = [];
-
-let selectedImg;
-
-// Click Next Button Function
-
-function nextImg() {
-	if (selectedImg < 0) return;
-	let number = Number(selectedImg);
-	let higestCount = Number(imagesArray.length - 1);
-	if (number === higestCount) {
-		number = 0;
-	} else {
-		number++;
-	}
-	selectedImg = number;
-	showImg.src = imagesArray[selectedImg];
-	addRemoveAnimation();
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
 }
 
-// Click Prev Button Function
-
-function prevImg() {
-	if (selectedImg < 0) return;
-	let number = Number(selectedImg);
-	let lowestCount = 0;
-	let higestCount = Number(imagesArray.length - 1);
-	if (number <= lowestCount) {
-		number = higestCount;
-	} else {
-		number--;
-	}
-	selectedImg = number;
-	showImg.src = imagesArray[selectedImg];
-	addRemoveAnimation();
-}
-
-// Initial Click Handler
-
-for (let image of images) {
-	imagesArray.push(image.src);
-	image.addEventListener("click", (e) => {
-		let img = e.target;
-		currentSelectedImg = imagesArray.indexOf(img.src);
-		mainImg.style.display = "block";
-		mainImg.classList.add("active");
-		showImg.src =
-			imagesArray[selectedImg !== undefined ? selectedImg : currentSelectedImg];
-		selectedImg = currentSelectedImg;
-	});
-}
-
-// Function for add and Remove Style Attribute
-
-function addRemoveAnimation() {
-	showImg.setAttribute("style", imgStyles);
-	setTimeout(() => {
-		showImg.setAttribute("style", "");
-	}, 500);
+// Get all images and insert the clicked image inside the modal
+// Get the content of the image description and insert it inside the modal image caption
+var images = document.getElementsByTagName('img');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var i;
+for (i = 0; i < images.length; i++) {
+   images[i].onclick = function(){
+       modal.style.display = "block";
+       modalImg.src = this.src;
+       modalImg.alt = this.alt;
+       captionText.innerHTML = this.nextElementSibling.innerHTML;
+   }
 }
